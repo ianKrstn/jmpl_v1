f5fb778a349ad299861f7ba3673a5e0557ff4338
Kristian Budi Pradana Putra <kristianbudipp@student.uns.ac.id>
2023-04-09 00:59:55 +0700

    nambah captcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,23 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('CaptchaCode') ? ' has-error' : '' }} row mb-3">
+                            <label for="CaptchaCode" class="col-md-4 col-form-label text-md-end">Captcha</label>
+
+
+                            <div class="col-md-6">
+                                {!! captcha_image_html('ContactCaptcha') !!}
+                                <input class="form-control" type="text" id="CaptchaCode" name="CaptchaCode" style="margin-top:5px;">
+
+
+                                @if ($errors->has('CaptchaCode'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('CaptchaCode') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">

871819de791714b15a8a63a721419e6e6c7ef9c9
Kristian Budi Pradana Putra <kristianbudipp@student.uns.ac.id>
2023-04-19 22:03:07 +0700

    nambah fitur 3x gagal login otomatis muncul captcha

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::middleware(['2fa']) -> group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
